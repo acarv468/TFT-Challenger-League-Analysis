37ed2f1943f32f97d4bb870c44f20d5853123643
acarv468 <acarv468@gmail.com>
2025-03-06 10:22:20 -0500

    dashboard awaiting set 14

--- challenger_dashboard.py
+++ challenger_dashboard.py
@@ -11,6 +11,8 @@ st.set_page_config(layout="wide")
 # Streamlit title
 st.title('TFT Challenger Dashboard')
 
+st.write("Teamfight Tactics (TFT) is a strategy game where players compete against each other in a series of rounds. Each round, players select units to fight against other players' units. The goal is to build a strong team composition and defeat all opponents to win the game. This dashboard provides insights into the top players in the Challenger league, as well as data on units, traits, and items used by players.")
+
 # Database connection parameters
 db_params = config()
 
@@ -64,15 +66,25 @@ units_data = get_data_from_db(units_query)
 traits_data = get_data_from_db(traits_query)
 challenger_data = get_data_from_db(challenger_query)
 
+# Remove the 'TFT13_' prefix and capitalize the first letter of each character_id entry
+units_data['character_id'] = units_data['character_id'].str.replace('TFT13_', '').str.capitalize()
+units_data['character_id'] = units_data['character_id'].str.replace('Tft13_', '').str.capitalize()
+
+traits_data['trait_name'] = traits_data['trait_name'].str.replace('TFT13_', '').str.capitalize()
+
 # Drop duplicates based on puuid and date
+challenger_data['date'] = challenger_data['date'].dt.strftime('%Y-%m-%d')
 challenger_data = challenger_data.drop_duplicates(subset=['puuid', 'date'])
 
+# Select the most recent date for each riotidgamename
+most_recent_data = challenger_data.loc[challenger_data.groupby('riotidgamename')['date'].idxmax()]
+
 # Create leaderboard for top 10 riotidgamenames by leaguepoints
-leaderboard_data = challenger_data.sort_values(by='leaguepoints', ascending=False).head(10)
+leaderboard_data = most_recent_data.sort_values(by='leaguepoints', ascending=False).head(10)
 leaderboard_data = leaderboard_data[['puuid', 'date', 'riotidgamename', 'leaguepoints', 'wins', 'losses']]
 
 # Convert the 'date' column to a string format that includes only the date part
-leaderboard_data['date'] = leaderboard_data['date'].dt.strftime('%Y-%m-%d')
+#leaderboard_data['date'] = leaderboard_data['date'].dt.strftime('%Y-%m-%d')
 
 # Calculate win percentage and format it
 leaderboard_data['win_pct'] = (leaderboard_data['wins'] / (leaderboard_data['wins'] + leaderboard_data['losses'])) * 100
@@ -100,7 +112,7 @@ st.markdown("""
         padding-top: 12px;
         padding-bottom: 12px;
         text-align: left;
-        background-color: #4CAF50;
+        background-color: #1f77b4;
         color: white;
     }
     </style>
@@ -108,7 +120,28 @@ st.markdown("""
 
 # Display the leaderboard as a styled table
 st.markdown("<h2>Top 10 Players by League Points</h2>", unsafe_allow_html=True)
+st.write("The table below shows the top 10 players in the Challenger league based on league points.")
 st.markdown(leaderboard_data_display.to_html(classes='leaderboard-table', index=False), unsafe_allow_html=True)
+
+# Create a line graph for the top 10 riotidgamenames by leaguepoints
+top_10_puuids = leaderboard_data['puuid'].tolist()
+top_10_data = challenger_data[challenger_data['puuid'].isin(top_10_puuids)]
+
+# Convert the 'date' column to a string format that includes only the date part
+
+line_chart = px.line(
+    top_10_data,
+    x='date',
+    y='leaguepoints',
+    color='riotidgamename',
+    title='League Points Over Time for Top 10 Players',
+    labels={'date': 'Date', 'leaguepoints': 'League Points', 'riotidgamename': 'Player'}
+)
+line_chart.update_layout(xaxis_title='Date', yaxis_title='League Points')
+line_chart.update_xaxes(tickformat='%Y-%m-%d', tickmode='linear')
+
+# Display the line chart in Streamlit
+st.plotly_chart(line_chart, use_container_width=True)
 
 # Create two columns for filters
 filter_col1, filter_col2 = st.columns(2)
@@ -138,19 +171,22 @@ if selected_player != 'All':
     filtered_units_data = filtered_units_data[filtered_units_data['puuid'] == selected_puuid]
     filtered_traits_data = filtered_traits_data[filtered_traits_data['puuid'] == selected_puuid]
 
-# Count the occurrences of each character_id
-character_counts = filtered_units_data['character_id'].value_counts().reset_index()
-character_counts.columns = ['character_id', 'count']
+# Count the occurrences of each character_id and tier
+character_tier_counts = filtered_units_data.groupby(['character_id', 'tier']).size().reset_index(name='count')
 
-# Create bar chart for the count of each character_id
+# Sort the character_tier_counts DataFrame by count in descending order
+character_tier_counts = character_tier_counts.sort_values(by='count', ascending=False)
+
+# Create stacked bar chart for the count of each character_id with tier as color
 bar_chart1 = px.bar(
-    character_counts,
+    character_tier_counts,
     x='character_id',
     y='count',
+    color='tier',
     title='Most Used Units',
-    labels={'count': 'Count', 'character_id': 'Unit ID'}
+    labels={'count': 'Times Used', 'character_id': 'Unit', 'tier': 'Tier'}
 )
-bar_chart1.update_layout(xaxis_title='Unit ID', yaxis_title='Count', barmode='group')
+bar_chart1.update_layout(xaxis_title='Unit', yaxis_title='Times Used', barmode='stack')
 
 # Count the occurrences of each trait_name
 trait_counts = filtered_traits_data['trait_name'].value_counts().reset_index()
@@ -162,7 +198,7 @@ bar_chart2 = px.bar(
     x='trait_name',
     y='count',
     title='Count of Trait',
-    labels={'count': 'Count', 'trait_name': 'Trait'}
+    labels={'count': 'Times Used', 'trait_name': 'Trait'}
 )
 bar_chart2.update_layout(xaxis_title='Trait', yaxis_title='Count', barmode='group')
 
@@ -172,11 +208,14 @@ col1.plotly_chart(bar_chart1, use_container_width=True)
 col2.plotly_chart(bar_chart2, use_container_width=True)
 
 # Single-select filter for character_id
-selected_character = st.selectbox('Select Unit', character_counts['character_id'])
+selected_character = st.selectbox('Select Unit', character_tier_counts['character_id'])
 
 # Filter items_data based on selected character_id
 items_data = filtered_units_data.explode('itemnames')
 filtered_items_data = items_data[items_data['character_id'] == selected_character]
+
+filtered_items_data['itemnames'] = filtered_items_data['itemnames'].str.replace('TFT_Item_', '').str.capitalize()
+filtered_items_data['itemnames'] = filtered_items_data['itemnames'].str.replace('Tft13_item_', '').str.capitalize()
 
 # Count the occurrences of each item for the selected character_id
 items_counts = filtered_items_data['itemnames'].value_counts().reset_index()
@@ -192,12 +231,36 @@ bar_chart3 = px.bar(
     x='itemnames',
     y='count',
     title=f'Top {top_n} Most Often Used Items By Unit: {selected_character}',
-    labels={'count': 'Count', 'itemnames': 'Item Names'}
+    labels={'count': 'Times Used', 'itemnames': 'Item'}
 )
-bar_chart3.update_layout(xaxis_title='Item Names', yaxis_title='Count', barmode='group', height=600)
+bar_chart3.update_layout(xaxis_title='Item', yaxis_title='Times Used', barmode='group', height=600)
 
-# Display the bar chart in Streamlit
-st.plotly_chart(bar_chart3, use_container_width=True)
+# Filter units_data to include only rows with the same puuid and match_id as the selected character
+selected_puuid_match_id = filtered_units_data[filtered_units_data['character_id'] == selected_character][['puuid', 'match_id']]
+team_comp_data = units_data.merge(selected_puuid_match_id, on=['puuid', 'match_id'])
+
+# Count the occurrences of each character_id in the team comp data
+team_comp_counts = team_comp_data['character_id'].value_counts().reset_index()
+team_comp_counts.columns = ['character_id', 'count']
+
+# Remove the selected character from the team comp counts
+team_comp_counts = team_comp_counts[team_comp_counts['character_id'] != selected_character]
+team_comp_counts = team_comp_counts.head(10)
+
+# Create bar chart for the count of each character_id in the team comp
+team_comp_chart = px.bar(
+    team_comp_counts,
+    x='character_id',
+    y='count',
+    title=f'Team Composition for Selected Unit: {selected_character}',
+    labels={'count': 'Times Used', 'character_id': 'Unit'}
+)
+team_comp_chart.update_layout(xaxis_title='Unit', yaxis_title='Times Used', barmode='group', height=600)
+
+# Display the item chart and team comp chart in two columns
+col1, col2 = st.columns(2)
+col1.plotly_chart(bar_chart3, use_container_width=True)
+col2.plotly_chart(team_comp_chart, use_container_width=True)
 
 # Display the filtered data in Streamlit
 st.write(filtered_units_data)
